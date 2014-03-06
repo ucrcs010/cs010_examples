d972cd3cea25f904fb07c9d260541e489904c4a2
Brian Linard <blinard@cs.ucr.edu>
2014-03-06 18:30:11 -0500

    latest changes

--- vectors/rainfall.cpp
+++ vectors/rainfall.cpp
@@ -8,9 +8,17 @@
  
 
 /// @brief function to read in data to populate vector (fillVector)
-/// file format: integer number of data items (n), followed by n doubles
+///     if the input comes from a file by input redirection,
+///     the file format will be a number of doubles, separated by spaces;
+///     if the input comes from keyboard, end of data will be signalled
+///     by ctrl-D (EoF symbol).
+///     
 /// @param v - vector<double> by reference
 /// @return void
+/// precondition: (i.e. the user has to abide by this!):
+///     the vector argument will be empty
+/// postcondition: (i.e. the function promises that this will happen!):
+///     the vector will be populated with the values input
  
 
 /// @brief function to find max value in a vector of doubles (maxValue)
@@ -22,15 +30,19 @@ int main()
 {
     double maxRainfall;
     
-    vector<double> rainfall(12);
-  
-    // invoke displayVector
+    vector<double> rainfall; // vector is initially empty!
     
-    // invoke fillVector
+    // invoke fillVector:
     
-    // invoke displayVector
     
-    // invoke maxValue
+    cout << "\nRainfall by month:" << endl;
+    // invoke displayVector:
+    
+    
+    // invoke maxValue:
+    
+    
+    cout << "Max monthly rainfall was: " << maxRainfall << endl;
     
     return 0;
 }
