b6caaea1817a5fbb7853e37d3d2f3e8524e455f1
Brian Linard <blinard@cs.ucr.edu>
2014-03-06 17:40:25 -0500

    vector function expamples

---
+++ vectors/rainfall.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+/// @brief function to display contents of vector (displayVector)
+/// @param v - vector<double> by const reference
+/// @return void
+ 
+
+/// @brief function to read in data to populate vector (fillVector)
+/// file format: integer number of data items (n), followed by n doubles
+/// @param v - vector<double> by reference
+/// @return void
+ 
+
+/// @brief function to find max value in a vector of doubles (maxValue)
+/// @param v - vector<double> by const reference
+/// @return double - max value
+
+
+int main()
+{
+    double maxRainfall;
+    
+    vector<double> rainfall(12);
+  
+    // invoke displayVector
+    
+    // invoke fillVector
+    
+    // invoke displayVector
+    
+    // invoke maxValue
+    
+    return 0;
+}
+
+
+// define displayVector:
+
+// define fillVector:
+
+// define maxValue:
