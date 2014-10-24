e223f44f55c479dc14b57820fe848f992d325c54
Adam <akoeh001@cs.ucr.edu>
2014-10-24 23:21:21 +0000

    simple arirthmetic examples with various variable types

---
+++ arithmetic/arithmetic_sect003.cpp
@@ -0,0 +1,43 @@
+/// @file arithmetic.cpp
+/// @author Adam Koehler
+/// @date October 13, 2014
+/// @brief Code snippets and comments utilized in lecture on 10/13/14 to 
+///        discuss arithmetic with variables.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int numUsers; //define
+    // numUsers = 0; // assign
+    
+
+    // x = 0; // origin
+    
+    // int numUsers2 = 0, numUsers = 4;
+    
+    // string myS; //default ""
+    
+    // 2 / 3 ----> 0
+    // ---------
+    // 2.0 / 3 -----> .6666666667
+    // floating point & anything -> floating path math
+    
+    
+    // 3 / 2 + 7 / 10 * 5.0 --------> 1 + 0.000 ------> 1.0
+    
+    // // character math uses + / -
+    // char c = 'c';
+    // c = c+1;
+    
+    // // concatenation, always starts with a string variable
+    // string s = "hello";
+    // string q = "world";
+    // s = s + " " + q;
+}
+
+
+ 
+
