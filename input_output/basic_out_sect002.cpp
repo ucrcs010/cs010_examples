b508745c3cf77fc85cb7e83f0ca56d4855d4ac97
Adam <akoeh001@cs.ucr.edu>
2015-01-09 19:30:35 +0000

    basic out

---
+++ input_output/basic_out_sect002.cpp
@@ -0,0 +1,5 @@
+/// @file basic_out.cpp
+/// @author Adam Koehler
+/// @date January 9, 2015
+/// @brief Example utilized in class on January 9, 2015 to discuss
+///         the basics of a program and basic output in C++.
