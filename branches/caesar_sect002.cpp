82f74be7ceacebe0b0e618751f107108cfae90c0
Adam <akoeh001@cs.ucr.edu>
2015-01-30 22:25:08 +0000

    basic caesar cipher program skeleton

---
+++ branches/caesar_sect002.cpp
@@ -0,0 +1,30 @@
+/// @file caesar.cpp
+/// @author Adam Koehler
+/// @date January 30, 2015
+/// @brief Code snippets and comments used to discuss strings and
+///        string member functions within encryption/decryption
+
+
+// In cryptography, a Caesar cipher is also known as 
+//  the shift cipher. This cipher is one of the simplest and most 
+//  widely known encryption techniques. 
+// Caesar cipher is a type of substitution cipher in which 
+//  each letter in the plaintext is replaced by a letter 
+//  some fixed number of positions down the alphabet. 
+// For example, with a left shift of -3, 
+//  D would be replaced by A, 
+//  E would become B, and so on. 
+// To decrypt, you simply shift backwards the same shift amount.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Assume lowercase letters and word size of 3.
+    // Implement caesar cipher printing out the encrypted word and
+    // then do decryption case printing out decrypted word at end 
+    // If your algorithm is correct, the user's word should be output at the end
+
+    return 0;
+}
