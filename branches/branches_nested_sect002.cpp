8909db34e51f3354f70ee0b22f28f71e0aea37fd
Adam <akoeh001@cs.ucr.edu>
2015-01-30 22:23:16 +0000

    updated to include dependent versus independent questions and combining if statements

--- branches/branches_nested_sect002.cpp
+++ branches/branches_nested_sect002.cpp
@@ -11,7 +11,49 @@ int main()
 {
     // three people, personA, personB, and personC
     //  who is the tallest?
+
+    // three types of questions:
+    //  dependent (true for initial) - compound using &&
+    //  dependent (false for initial) - else if statement
+    //  independent multiple sequential ifs - compound using ||
+
+
+    // independent questions - if (a > c || a > b)
+    if (a > c)
+    {
+        cout << " a is bigger "; 
+        
+    }
+    if (a > b)
+    {
+        cout << " a is bigger ";
+        cout << "this too";
+        
+    }
+    // becomes
+    if (a>c || a>b)
+    {
+        cout << "a is bigger ";
+    }
+    if (a > b)
+    {
+        cout << "this too";
+    }
+    }
+
     
+    // don't write blank blocks, dependent with initial false answer
+    if (a > c)
+    {
+        // do nothing        
+    }
+    else if (a > b) // avoid blank block using if (!(a > c) && a > b)
+    {
+        // do something
+    }
+    
+    
+    // dependent questions (truth) if (personA > persronB && personA > personC)
     if (personA > personB) // Q1
     {
         if(personA > personC) // Q2, only asked when Q1 is true (yes)
