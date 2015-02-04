518d0c7246ba73f5fc0a8fbb7a3db37a0e8b082a
Adam <akoeh001@cs.ucr.edu>
2015-02-04 17:18:00 +0000

    caesar cipher initial push

--- branches/caesar_sect002.cpp
+++ branches/caesar_sect002.cpp
@@ -1,6 +1,6 @@
 /// @file caesar.cpp
 /// @author Adam Koehler
-/// @date January 30, 2015
+/// @date February 2, 2015
 /// @brief Code snippets and comments used to discuss strings and
 ///        string member functions within encryption/decryption
 
@@ -22,9 +22,80 @@ using namespace std;
 int main()
 {
     // Assume lowercase letters and word size of 3.
-    // Implement caesar cipher printing out the encrypted word and
-    // then do decryption case printing out decrypted word at end 
-    // If your algorithm is correct, the user's word should be output at the end
+    // Implement caesar cipher printing out the encrypted word 
+    // and then do decryption case printing out decrypted word 
+    // at end. If your algorithm is correct, the user's word 
+    // should be output at the end
+    int shift = 0;
+    string option; 
+    string word;
+    
+    // encryption or decryption?
+    cout << "Would you like to encrypt or decrypt?";
+    cin >> option;
+    cout << endl;
+    
+    // acquire shift value
+    cout << "What is the caesar shift value? ";
+    cin >> shift;
+    cout << endl;
+    
+    // acquire the word to encrypt
+    cout << "What word would you like to encrypt? ";
+    cin >> word;
+    cout << endl;    
+    
+    // the shift value should be no greater than shifting 
+    // across the entire alphabet from first to last letter
+    int maxShift = 'z' - 'a';
+    
+    // update the shift value to be within max range, [0, maxShift]
+    shift = shift % maxShift;
+    
+    // Q: How do we know maxShift and modifying shift works?
+    // Q: How can we show it? (not necessarily prove conclusively)
+    
+    
+    // Q: How can we convert the following loop to a for loop?
+    // Q: Why should we be using a for loop?
+    
+    
+    if ("encrypt" == option)
+    {
+        // go over the entire word and convert the letters
+        int i = 0;
+        while (i < 3)
+        {
+            word.at(i) = word.at(i) + shift;
+            
+            // when converting to a loop we do not need the individual updates
+            //      as a single index is handled per iteration of the loop
+            // word.at(1) = word.at(1) + shift;
+            // word.at(2) = word.at(2) + shift;
+            
+            if (word.at(i) > 'z')
+            {
+                word.at(i) = word.at(i) - 26;
+            }
+            i++;
+        }
+    }
+    
+    // Q: How can we add decryption?
+    
+    
+    // Output the phrase for option chosen by the user
+    if ("encrypt" == option)
+    {
+        cout << "Encrypted word: ";
+    }
+    else
+    {
+        cout << "Decrypted word: ";
+    }
+    
+    // Output the converted word
+    cout << word << endl;
 
     return 0;
 }
