ce8a14322c0dcb929cc760235c3084bb9352d541
Adam <akoeh001@cs.ucr.edu>
2015-02-20 19:17:11 +0000

    overlap in class exercise introduction

---
+++ loops/overlapIntro_sect002.cpp
@@ -0,0 +1,19 @@
+// what is the point of the assignment (what does it do)
+find sequences that match in DNA
+
+
+// which page is the algorithm on ?
+verbal description - your assignment page
+visual description - examples page
+
+// what string functions do we use?
+hints & tips
+size()
+substr()
+find()
+
+
+// will we require loops, or other C++ structures
+while() --> go over list of words
+if-else
+for --> string iteration
