5bacd844270555bc7b415ae0860175cf7caace48
Adam <akoeh001@cs.ucr.edu>
2015-02-20 21:05:10 +0000

    first three word counting functions

--- functions/wordCounting_sect002.cpp
+++ functions/wordCounting_sect002.cpp
@@ -4,20 +4,65 @@
 /// @brief Code snippets and comments used to discuss and write functions.
 
 // First:
-// Given a character, write a predicate function that returns whether the 
-//  provided character is a vowel (y counts as a vowel)
+// Given a character, write a predicate function that returns 
+//  whether the provided character is a vowel 
+//  (y counts as a vowel)
+bool isVowel(char c)
+{
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || 
+        c == 'u' || c == 'y')
+    {
+        return true;
+    }
+
+    return false;
+
+}
 
 // Second: 
-// Given a word, write a function that will count & return the number of 
-//  vowels that are contained within the provided word
+// Given a word, write a function that will count & return 
+//  the number of vowels that are contained within the 
+//  provided word
+int countVowels(string word)
+{
+    int count = 0;
+    
+    for (int i=0; i < word.size(); i++)
+    {
+        if (isVowel(word.at(i)))
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
 
 // Third: 
-// Given a word, write a function that will count & return the number of 
-//  consonants that are contained within the provided word
+// Given a word, write a function that will count & return 
+//  the number of consonants that are contained within the 
+//  provided word
+int countCons(string word)
+{
+    int count = 0;
+    
+    count = word.size() - countVowels(word);
+    
+    return count; 
+}
 
 // Fourth: 
-// Write a function called testCons that tests the consonant counting 
-//  function with several different possible word scenarios
+// Write a function called testCons that tests the consonant 
+//  counting function with several different possible word 
+//  scenarios
+void testCons()
+{
+    
+    
+}
+
+
+
 
 // Lastly: 
 // Write a main function that iterates over an input file of words and outputs
