b50432bfb891fa199f9ac5535d4944cf74930d22
Adam <akoeh001@cs.ucr.edu>
2016-09-30 01:56:10 +0000

    output

---
+++ input_output/basic_out.cpp
@@ -0,0 +1,27 @@
+/// @file basic_out.cpp
+/// @author Adam Koehler
+/// @date September 27, 2016
+/// @brief Example utilized in class on September 27, 2016 
+///     to discuss the basics of a program and basic 
+///     output in C++.
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // keyword for output - cout 
+    // operator - << 
+    // all statements end in a semicolon
+    cout << "Hello World" << endl << 3 
+        << " sdf sdfsdf sdfsdfHi how sdfsdfsdfsdfare you";
+        
+    // multi line comment
+    /* 
+    hello
+    
+    */
+    
+    return 0;
+}
