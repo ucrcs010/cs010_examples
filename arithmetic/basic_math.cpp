de03a26abb53f357d9ee7478e08d7a52597fbb3a
Adam <akoeh001@cs.ucr.edu>
2016-10-05 01:27:43 +0000

    base files for 10-4-16 lecture

---
+++ arithmetic/basic_math.cpp
@@ -0,0 +1,15 @@
+/// @file basic_math.cpp
+/// @author Adam Koehler
+/// @date October 4, 2016
+/// @brief Code snippets and comments utilized in lecture on 10/4/16 to 
+///        discuss basic math expressions with different datatypes.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
---
+++ variables/casting.cpp
@@ -0,0 +1,15 @@
+/// @file casting.cpp
+/// @author Adam Koehler
+/// @date October 4, 2016
+/// @brief Code snippets and comments utilized in lecture on 10/04/16 to 
+///        discuss casting between variable types.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
