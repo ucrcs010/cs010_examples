e162d06051142cf695eed7f209d11eae6ae6aa38
Adam <akoeh001@cs.ucr.edu>
2016-10-26 03:39:54 +0000

    updated with code produced in lecture

--- loops/nested_loops.cpp
+++ loops/nested_loops.cpp
@@ -11,32 +11,62 @@ int main()
     int sideSize = 4;
     
     // take in size of side
-    // cin >> sideSize;
+    cin >> sideSize;
     
     // draw a square to output using nested loops
     // use for loops because we know the iteration count
     // nested means one loop inside another
+    //
     //  * * * * 
     //  * * * *
     //  * * * *
     //  * * * *
+    for(int r=0; r < sideSize; r++)
+    {
+        for(int c=0; c < sideSize; c++)
+        {
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
     cout << endl << endl;
     
     // draw a right isosceles triangle
     //
-    //  * * * * 
-    //  * * * 
-    //  * *
-    //  *
+    //  * * * * 4 - 0
+    //  * * *   4 - 1
+    //  * *     4 - 2
+    //  *       4 - 3
+    for(int r=0; r < sideSize; r++)
+    {
+        for(int c=0; c < sideSize - r; c++)
+        {
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
     cout << endl << endl; 
     
     // draw a right isosceles triangle
     // other half of square
     //
     //        * 
-    //      * *
-    //    * * *
-    //  * * * *
+    //      * * 
+    //    * * *   
+    //  * * * * 
+    for (int r=1; r <= sideSize; r++)
+    {
+        for (int c=1; c <= sideSize - r ; c++)
+        {
+            cout << '*' << ' ';
+        }
+        
+        for(int c=1; c <= r ; c++)
+        {
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
         
     
     return 0;
