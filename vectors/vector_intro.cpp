b6750d5cf485aad3c2319d1692cc7a3948dc795e
Adam <akoeh001@cs.ucr.edu>
2016-11-23 04:00:02 +0000

    vectors intro commented

--- vectors/vector_intro.cpp
+++ vectors/vector_intro.cpp
@@ -1,15 +1,21 @@
+/// @file vector_intro.cpp
+/// @author Adam Koehler
+/// @date November 22, 2016
+/// @brief Code to discuss using vectors
+
+
 // what is a vector
-variable that is a colletion a similar variables
+// variable that is a colletion a similar variables
 
 // how many types can a vector hold?
-one
+// one
 
 // remember the library
 #include <vector>
 
 // declare vector
-vector<double> v(4);
-vector<int> v1;
+// vector<double> v(4);
+// vector<int> v1;
 
 // vector are always initialized, usually 0
 // string -- is also initialed ""
@@ -19,23 +25,23 @@ vector<int> v1;
 // increase size by 1
 // then put 2 in last spot
 // mostly used when starting with empty vect
-v1.push_back(2); 
+// v1.push_back(2); 
 
 // used when we have slots already
-v.at(0) = 4;
-v.at(3) = 5.4;
-v.push_back(5); // 4, 0, 0, 5.4, 5
+// v.at(0) = 4;
+// v.at(3) = 5.4;
+// v.push_back(5); // 4, 0, 0, 5.4, 5
 // loop over all the elements and set
 
 // (size, start value)
-vector<double> v3(4, 5);
+// vector<double> v3(4, 5);
 
 // defining functions with vectors
 // vector<int> hasTrue
 //     (const vector<bool> &myBools);
 bool hasTrue(const vector<bool> &myBools)
 // constant or not for reference parameters
-// this is strings too
+// this is for strings too, const or not but always reference
 {
     int sz = myBools.size()
     for (int i=0; i < sz; ++i)
@@ -75,9 +81,3 @@ int main()
     
     return; 
 }
-
-
-
-
-
-
